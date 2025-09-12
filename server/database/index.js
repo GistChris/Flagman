6d6a039209582ae254de08f0b8bea0a9b84c4833
GistChris <chris.69@mail.ru>
2025-09-11 21:10:23 -0400

    connected MongoDB

--- server/database/index.js
+++ server/database/index.js
@@ -2,21 +2,27 @@ import mongoose from "mongoose";
 
 const protocol = "mongodb+srv";
 // const url = "cluster0.c0jhclh.mongodb.net";
-const url = "cluster0.rbklztt.mongodb.net";
+//mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })  
+//mongodb+srv://alexander:<db_password>@cluster2.yiihhb5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2
+
 
 const params = "?retryWrites=true&w=majority";
-const username = "User";
-const password = "pw4User$";
+//const url = "cluster0.rbklztt.mongodb.net";
+// const username = "User";
+// const password = "pw4User$";
+const url = "cluster2.yiihhb5.mongodb.net";
+const username = "alexander";
+const password = "Tamara";
 const database = "DAWTA-AUTH";
 
 export const connectionString = `${protocol}://${username}:${password}@${url}/${database}${params}`;
 
 const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+  // useFindAndModify: false,
+  //useCreateIndex: true,
+  //useFindAndModify: false,
 };
 
 export const connect = (callback) =>
